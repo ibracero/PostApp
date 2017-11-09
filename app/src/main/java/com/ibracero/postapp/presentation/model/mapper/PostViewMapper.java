0a804c980cec7a41195eb94565e1e4bdbfc118f5
Imanol Bracero Armenteros <donbasket50@gmail.com>
2017-11-09 21:07:54 +0100

    Added PostViewMapper

--- app/src/main/java/com/ibracero/postapp/presentation/model/mapper/PostViewMapper.java
+++ app/src/main/java/com/ibracero/postapp/presentation/model/mapper/PostViewMapper.java
@@ -1,4 +1,30 @@
 package com.ibracero.postapp.presentation.model.mapper;
 
-public class PostViewMapper {
+import com.ibracero.postapp.domain.model.PostModel;
+import com.ibracero.postapp.domain.model.mapper.Mapper;
+import com.ibracero.postapp.presentation.model.PostViewModel;
+
+import javax.inject.Inject;
+
+public class PostViewMapper extends Mapper<PostModel, PostViewModel> {
+
+    @Inject
+    public PostViewMapper() {
+    }
+
+    @Override
+    protected PostViewModel transform(PostModel model) {
+        return new PostViewModel.Builder()
+                .id(model.getId())
+                .title(model.getTitle())
+                .build();
+    }
+
+    @Override
+    protected PostModel untransform(PostViewModel model) {
+        return new PostModel.Builder()
+                .id(model.getId())
+                .title(model.getTitle())
+                .build();
+    }
 }
