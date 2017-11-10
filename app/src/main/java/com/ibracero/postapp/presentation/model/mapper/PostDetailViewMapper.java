5fe7ebff5302fbdea423342be8ab59e1cba437af
Imanol Bracero Armenteros <donbasket50@gmail.com>
2017-11-10 13:39:13 +0100

    Added post detail view mapper

--- app/src/main/java/com/ibracero/postapp/presentation/model/mapper/PostDetailViewMapper.java
+++ app/src/main/java/com/ibracero/postapp/presentation/model/mapper/PostDetailViewMapper.java
@@ -1,4 +1,37 @@
 package com.ibracero.postapp.presentation.model.mapper;
 
-public class PostDetailViewMapper {
+import com.ibracero.postapp.Constants;
+import com.ibracero.postapp.domain.model.PostModel;
+import com.ibracero.postapp.domain.model.mapper.Mapper;
+import com.ibracero.postapp.presentation.model.PostDetailViewModel;
+
+import javax.inject.Inject;
+
+public class PostDetailViewMapper extends Mapper<PostModel, PostDetailViewModel> {
+
+    @Inject
+    public PostDetailViewMapper() {
+    }
+
+    @Override
+    protected PostDetailViewModel transform(PostModel model) {
+        return new PostDetailViewModel.Builder()
+                .postTitle(model.getTitle())
+                .postBody(model.getBody())
+                .writerName(model.getWriter() != null
+                        ? model.getWriter().getUsername()
+                        : "")
+                .writerImageUrl(model.getWriter() != null
+                        ? Constants.IMAGE_GENERATOR_URL + model.getWriter().getEmail()
+                        : "")
+                .commentCounter(model.getComments() != null
+                        ? String.valueOf(model.getComments().size())
+                        : "")
+                .build();
+    }
+
+    @Override
+    protected PostModel untransform(PostDetailViewModel model) {
+        return null;
+    }
 }
